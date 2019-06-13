f36859aec267abe1b0a188bde9697ab7bcb689cc
Diego Souza <dee-souza@hotmail.com>
2019-06-13 13:51:14 -0300

    Feat: Icons from page and pages feed and header

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,8 +1,74 @@
 import React, { Component } from "react";
+import "./Feed.css";
+
+import more from "../assets/more.svg";
+import like from "../assets/like.svg";
+import comment from "../assets/comment.svg";
+import send from "../assets/send.svg";
 
 class Feed extends Component {
   render() {
-    return <h1>Oi</h1>;
+    return (
+      <section id="post-list">
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Diego Souza</span>
+              <span className="place">Piracicaba, SP</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+          </header>
+
+          <img src="http://localhost:3333/files/1.jpg" alt="" />
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt="Like" />
+              <img src={comment} alt="Comment" />
+              <img src={send} alt="Send" />
+            </div>
+
+            <strong>60 curtidas</strong>
+
+            <p>
+              Um post muito legal da omnistack, criando uma página estilo
+              Instagram.
+              <span>#top #instagram #rocketseat</span>
+            </p>
+          </footer>
+        </article>
+
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Diego Souza</span>
+              <span className="place">Piracicaba, SP</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+          </header>
+
+          <img src="http://localhost:3333/files/1.jpg" alt="" />
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt="Like" />
+              <img src={comment} alt="Comment" />
+              <img src={send} alt="Send" />
+            </div>
+
+            <strong>60 curtidas</strong>
+
+            <p>
+              Um post muito legal da omnistack, criando uma página estilo
+              Instagram.
+              <span>#top #instagram #rocketseat</span>
+            </p>
+          </footer>
+        </article>
+      </section>
+    );
   }
 }
 
